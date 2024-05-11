0b2bfe00a5fce63b6831b6acd5dd3be6a64530ad
Alexander Stage <abs@stubbe.dev>
2024-05-11 13:45:31 +0200

    access control

--- src/app/services/wine.service.ts
+++ src/app/services/wine.service.ts
@@ -6,38 +6,38 @@ import { Wine } from '../models/wine';
 
 @Injectable({
   providedIn: 'root'
-  
+
 })
 
 export class WineService {
   authHeader: string = "Basic am9obi5kb2U6VmVyeVNlY3JldCE=";
-  private baseUrl: string = 'https://localhost:7196/api/Wines'; 
+  private baseUrl: string = 'https://localhost:7196/api/Wines';
 
   constructor(private http: HttpClient) { }
 
   getWines(): Observable<Wine[]> {
-    const headers = new HttpHeaders({ 'Authorization': this.authHeader });
+    const headers = new HttpHeaders({ 'Authorization': this.authHeader, 'Access-Control-Allow-Headers': 'Content-Type','Access-Control-Allow-Origin': '*' });
     return this.http.get<Wine[]>(this.baseUrl, { headers: headers });
   }
 
   getWineById(id: number): Observable<Wine> {
-    const headers = new HttpHeaders({ 'Authorization': this.authHeader });
+    const headers = new HttpHeaders({ 'Authorization': this.authHeader, 'Access-Control-Allow-Headers': 'Content-Type','Access-Control-Allow-Origin': '*' });
     return this.http.get<Wine>(`${this.baseUrl}/${id}`, { headers: headers });
   }
 
   updateWine(wine: Wine): Observable<Wine> {
-    const headers = new HttpHeaders({ 'Authorization': this.authHeader });
+    const headers = new HttpHeaders({ 'Authorization': this.authHeader, 'Access-Control-Allow-Headers': 'Content-Type','Access-Control-Allow-Origin': '*' });
     const url = `${this.baseUrl}/${wine.id}`;
     return this.http.put<Wine>(url, wine, { headers: headers });
   }
 
   addWine(wine: Wine): Observable<Wine> {
-    const headers = new HttpHeaders({ 'Authorization': this.authHeader });
+    const headers = new HttpHeaders({ 'Authorization': this.authHeader, 'Access-Control-Allow-Headers': 'Content-Type','Access-Control-Allow-Origin': '*' });
     return this.http.post<Wine>(this.baseUrl, wine, { headers: headers });
   }
 
   deleteWine(id: number): Observable<string> {
-    const headers = new HttpHeaders({ 'Authorization': this.authHeader });
+    const headers = new HttpHeaders({ 'Authorization': this.authHeader, 'Access-Control-Allow-Headers': 'Content-Type','Access-Control-Allow-Origin': '*' });
     return this.http.delete<string>(`${this.baseUrl}/${id}`, { headers: headers, responseType: 'text' as 'json' });
   }
 }
